0a0bdca99010496f0e3ec225a95d74a08a567520
reia_kito <reia_kito@mail.toyota.co.jp>
2025-05-28 14:03:57 +0900

    Score.jsx　途中

--- front/src/Score.jsx
+++ front/src/Score.jsx
@@ -1,13 +1,37 @@
-// import { useState } from 'react'
-import './Score.css'
+import { useState, useEffect } from 'react';
+import './Score.css';
 
 function Score() {
+  const [score, setScore] = useState(0);
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  async function getScore() {
+    const responce = await fetch(`/api/scores/records/${year}-${month}-${day}`);
+    const todayScore = await responce;
+    console.log(responce);
+    console.log(todayScore);
+  }
+
+  useEffect(() => {
+    getScore();
+  }, []);
 
   return (
-    <div className='scorContainer'>
-        本日のハイスコア <u><b>78</b></u> pt
+    <div className="scorContainer">
+      本日のハイスコア{' '}
+      <u>
+        <b>{score}</b>
+      </u>{' '}
+      pt
+      <button onClick={getScore}>test</button>
     </div>
-  )
+  );
 }
 
 export default Score;
+
+//  /api/scores/records/:date 指定した日付
