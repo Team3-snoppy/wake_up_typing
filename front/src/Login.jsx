330f322c67cd1fca4e2cc17383003f6d25229da8
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-28 11:57:52 +0900

    login builded

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -2,7 +2,10 @@ import { useRef, useState } from 'react';
 import './Login.css';
 
 function Login() {
-  const [loginForm, setLoginForm] = useState('block')
+  const [loginForm, setLoginForm] = useState('block');
+  const [widthInput, setWidthInput] = useState('30%');
+  const [widthButton, setButton] = useState('70%');
+  const [loginState, setLoginState] = useState('login');
 
   const refUser = useRef(null);
   const refPass = useRef(null);
@@ -19,18 +22,21 @@ function Login() {
       .then((res) => res.json())
       .then((data) => console.log(data));
       setLoginForm('none');
+      setWidthInput('0%');
+      setButton('100%');
+      setLoginState('logout');
   }
 
   return (
     <div className="formContainer">
-      <div className="inputForm" style={{}}>
+      <div className="inputForm" style={{width: widthInput}}>
         <input ref={refUser} className="input" placeholder="use name" style={{display: loginForm}}></input>
         <br />
         <input ref={refPass} className="input" placeholder="password" style={{display: loginForm}}></input>
       </div>
-      <div className="submitButton">
+      <div className="submitButton" style={{width: widthButton}}>
         <button className="button" onClick={login}>
-          login
+          {loginState}
         </button>
         <button className="button">sign up</button>
       </div>
