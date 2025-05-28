e766ce1c18f0e828b3211e0ca214ac182d9317f4
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-28 12:19:23 +0900

    isLogini flag

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -2,41 +2,45 @@ import { useRef, useState } from 'react';
 import './Login.css';
 
 function Login() {
-  const [loginForm, setLoginForm] = useState('block');
-  const [widthInput, setWidthInput] = useState('30%');
-  const [widthButton, setButton] = useState('70%');
-  const [loginState, setLoginState] = useState('login');
+  const [isLogin, setIsLogin] = useState(false);
+  // const [widthInput, setWidthInput] = useState('30%');
+  // const [widthButton, setButton] = useState('70%');
+  // const [loginState, setLoginState] = useState('login');
 
   const refUser = useRef(null);
   const refPass = useRef(null);
 
   async function login() {
-    console.log(refUser.current.value,refPass.current.value);
+    console.log(refUser.current.value, refPass.current.value);
     await fetch('/api/auth/login', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userName: refUser.current.value, password: refPass.current.value }),
+      body: JSON.stringify({
+        userName: refUser.current.value,
+        password: refPass.current.value,
+      }),
     })
       .then((res) => res.json())
       .then((data) => console.log(data));
-      setLoginForm('none');
-      setWidthInput('0%');
-      setButton('100%');
-      setLoginState('logout');
+    setIsLogin(true);
   }
 
   return (
     <div className="formContainer">
-      <div className="inputForm" style={{width: widthInput}}>
-        <input ref={refUser} className="input" placeholder="use name" style={{display: loginForm}}></input>
-        <br />
-        <input ref={refPass} className="input" placeholder="password" style={{display: loginForm}}></input>
-      </div>
-      <div className="submitButton" style={{width: widthButton}}>
+      {!isLogin ? (
+        <div className="inputForm">
+          <input ref={refUser} className="input" placeholder="use name"></input>
+          <br />
+          <input ref={refPass} className="input" placeholder="password"></input>
+        </div>
+      ) : (
+        ''
+      )}
+      <div className="submitButton">
         <button className="button" onClick={login}>
-          {loginState}
+          {!isLogin ? 'logoin' : 'logout'}
         </button>
         <button className="button">sign up</button>
       </div>
