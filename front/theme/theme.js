c9c3758c844386e914f752f9862519cfd8b5cee3
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-05-28 17:11:53 +0900

    app.jsxリファクタリング：themeフォルダに移動

---
+++ front/theme/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+import { blue, green, red, yellow } from '@mui/material/colors';
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: red[500],
+      },
+      secondary: {
+        main: blue[500],
+      },
+    },
+  });
+
+  export default theme
