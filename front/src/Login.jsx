558d154f3bc54ecf1fa1b68c8a605ddd1c7150af
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-28 10:25:49 +0900

    useRef

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -1,19 +1,33 @@
-// import { useState } from 'react'
+import { useRef } from 'react';
 import './Login.css';
 
 function Login() {
+  const refUser = useRef(null);
+  const refPass = useRef(null);
+
+  async function login() {
+    await fetch('/api/auth/login', {
+      method: 'post',
+      body: { userName: refUser, password: refPass },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
-      <div className='formContainer'>
-        <div className="inputForm">
-          <input className='input' placeholder='use name'></input>
-          <br />
-          <input className='input' placeholder='password'></input>
-        </div>
-        <div className="submitButton">
-          <button className='button'>login</button>
-          <button className='button'>sign up</button>
-        </div>
+    <div className="formContainer">
+      <div className="inputForm">
+        <input ref={refUser} className="input" placeholder="use name"></input>
+        <br />
+        <input ref={refPass} className="input" placeholder="password"></input>
       </div>
+      <div className="submitButton">
+        <button className="button" onClick={login}>
+          login
+        </button>
+        <button className="button">sign up</button>
+      </div>
+    </div>
   );
 }
 
