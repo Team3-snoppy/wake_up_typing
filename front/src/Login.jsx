da888ffc9bff2df0becc24cc823ee65b8c9e87a9
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-28 14:46:20 +0900

    sign up builded

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -45,6 +45,21 @@ function Login() {
       });
   }
 
+  async function signUp() {
+    await fetch('/api/auth/new-accounts', {
+      method: 'post',
+            headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: refUser.current.value,
+        password: refPass.current.value,
+      }),
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
+
   console.log(userInfo);
 
   return (
@@ -68,7 +83,7 @@ function Login() {
             logoout
           </button>
         )}
-        {!isLogin ? <button className="button">sign up</button> : ''}
+        {!isLogin ? <button className="button" onClick={signUp}>sign up</button> : ''}
       </div>
     </div>
   );
