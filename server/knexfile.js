743c3a091dacc7b6f4050e1f215deda5abed7488
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-28 18:07:46 +0900

    vite

--- server/knexfile.js
+++ server/knexfile.js
@@ -21,10 +21,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
       directory: 'db/migrations',
     },
