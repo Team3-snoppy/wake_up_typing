f3063de71844c9098494ea3810554b8a4b82cf33
reia_kito <reia_kito@mail.toyota.co.jp>
2025-05-29 14:29:04 +0900

    kito seedfile

--- server/db/seeds/005-Kito.js
+++ server/db/seeds/005-Kito.js
@@ -4,9 +4,18 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' },
+  await knex('words').insert([
+    { user_id: 4, word: 'あなたのドレス、とても素敵ですわ。' },
+    { user_id: 4, word: '今日は本当に素晴らしいお天気ですわね。' },
+    { user_id: 4, word: 'お茶会にお招きいただき、感謝いたしますわ。' },
+    { user_id: 4, word: 'お散歩に行くのが楽しみですわ。' },
+    { user_id: 4, word: 'この本は非常に興味深い内容でございますわ。' },
+    {
+      user_id: 4,
+      word: '素晴らしいパフォーマンスでしたわ。感動いたしました。',
+    },
+    { user_id: 4, word: 'お茶をお淹れいたしますわ。お待ちくださいませ。' },
+    { user_id: 4, word: '心温まるお話を聞かせて頂き、感謝も申し上げます。' },
+    { user_id: 4, word: 'この場所は本当に美しいですわね。' },
   ]);
 };
