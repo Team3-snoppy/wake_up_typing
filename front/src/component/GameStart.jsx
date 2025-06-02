cffc38ea4231663da249f41549fdf67135f038de
ruka <ruka_ootani@mail.toyota.co.jp>
2025-06-02 19:35:25 +0900

    game info get

--- front/src/component/GameStart.jsx
+++ front/src/component/GameStart.jsx
@@ -1,71 +1,94 @@
-import { Container, Button, RadioCardGroup, RadioCard, Text, Flex, Select, OptionGroup, Option } from '@yamada-ui/react';
-
+import { Container, Button, RadioCardGroup, RadioCard, Text, Flex, Select, Option } from '@yamada-ui/react';
+import { useRef, useState } from 'react';
 const GameStart = ({ setGameState }) => {
+	const gameStart = () => {
+		console.log('🍓 ~ GameStart ~ selectCategory:', selectCategory);
+		console.log('🍓 ~ GameStart ~ sleepHour:', sleepHour);
+		console.log('🍓 ~ GameStart ~ sleepMinutes:', sleepMinutes);
+		console.log('🍓 ~ GameStart ~ getUpHour:', getUpHour);
+		console.log('🍓 ~ GameStart ~ getUpMinutes:', getUpMinutes);
+
+		setGameState(1);
+	};
+	const [selectCategory, setSelectCategory] = useState('');
+	const [sleepHour, setSleepHour] = useState('');
+	const [sleepMinutes, setSleepMinutes] = useState('');
+	const [getUpHour, setGetUpHour] = useState('');
+	const [getUpMinutes, setGetUpMinutes] = useState('');
+
+	const categoryRadios = [
+		{ label: 'ガンダム', value: '1', description: 'わかる人にはわかるはず。朝から熱くいきましょう。' },
+		{ label: 'くるま', value: '2', description: 'わからないと失格ですね。何がとは言いませんが。' },
+		{ label: 'お嬢様', value: '3', description: 'ギャル？お嬢様？ちょっとお上品にいきましょうか' },
+		{ label: 'スポーツ', value: '4', description: '自分を鼓舞して一日頑張りましょう' },
+		{ label: 'ゲーム', value: '5', description: 'FROM,FPS' },
+	];
+
+	const selectMinutes = ['00', '10', '20', '30', '40', '50'];
+	const selectSleepHour = ['20', '21', '22', '23', '24', '1', '2', '3', '4'];
+	const selectGetUpHour = ['4', '5', '6', '7', '8', '9', '10'];
+
 	return (
 		<>
 			<Container>
-				<RadioCardGroup>
-					<RadioCard label="ガンダム" value="1" description="わかる人にはわかるはず。朝から熱くいきましょう。"></RadioCard>
-					<RadioCard label="くるま" value="1" description="わからないと失格ですね。何がとは言いませんが。"></RadioCard>
-					<RadioCard label="お嬢様" value="1" description="ギャル？お嬢様？ちょっとお上品にいきましょうか"></RadioCard>
-					<RadioCard label="スポーツ" value="1" description="自分を鼓舞して一日頑張りましょう"></RadioCard>
-					<RadioCard label="ゲーム" value="1" description="FROM,FPS"></RadioCard>
+				<RadioCardGroup onChange={setSelectCategory}>
+					{categoryRadios.map((categoryRadio) => {
+						return <RadioCard key={categoryRadio.value} label={categoryRadio.label} value={categoryRadio.value} description={categoryRadio.description}></RadioCard>;
+					})}
 				</RadioCardGroup>
 			</Container>
 			<Flex>
 				<Container>
 					<Text textAlign="left">入眠時間</Text>
 					<Flex>
-						<Select w="2xs" placeholder="--">
-							<Option value="20">20</Option>
-							<Option value="21">21</Option>
-							<Option value="22">22</Option>
-							<Option value="23">23</Option>
-							<Option value="24">24</Option>
-							<Option value="0">0</Option>
-							<Option value="1">1</Option>
-							<Option value="2">2</Option>
-							<Option value="3">3</Option>
-							<Option value="4">4</Option>
+						<Select onChange={setSleepHour} w="2xs" placeholder="--">
+							{selectSleepHour.map((hour) => {
+								return (
+									<Option key={hour} value={hour}>
+										{hour}
+									</Option>
+								);
+							})}
 						</Select>
 						<Text w="7xs">:</Text>
-						<Select w="2xs" placeholder="--">
-							<Option value="00">00</Option>
-							<Option value="10">10</Option>
-							<Option value="20">20</Option>
-							<Option value="30">30</Option>
-							<Option value="40">40</Option>
-							<Option value="50">50</Option>
+						<Select onChange={setSleepMinutes} w="2xs" placeholder="--">
+							{selectMinutes.map((minutes) => {
+								return (
+									<Option key={minutes} value={minutes}>
+										{minutes}
+									</Option>
+								);
+							})}
 						</Select>
 					</Flex>
 				</Container>
 				<Container>
 					<Text textAlign="left">起床時間</Text>
 					<Flex>
-						<Select w="2xs" placeholder="--">
-							<Option value="4">4</Option>
-							<Option value="5">5</Option>
-							<Option value="6">6</Option>
-							<Option value="7">7</Option>
-							<Option value="8">8</Option>
-							<Option value="9">9</Option>
-							<Option value="10">10</Option>
-							<Option value="11">11</Option>
+						<Select onChange={setGetUpHour} w="2xs" placeholder="--">
+							{selectGetUpHour.map((hour) => {
+								return (
+									<Option key={hour} value={hour}>
+										{hour}
+									</Option>
+								);
+							})}
 						</Select>
 						<Text w="7xs">:</Text>
-						<Select w="2xs" placeholder="--">
-							<Option value="00">00</Option>
-							<Option value="10">10</Option>
-							<Option value="20">20</Option>
-							<Option value="30">30</Option>
-							<Option value="40">40</Option>
-							<Option value="50">50</Option>
+						<Select onChange={setGetUpMinutes} w="2xs" placeholder="--">
+							{selectMinutes.map((minutes) => {
+								return (
+									<Option key={minutes} value={minutes}>
+										{minutes}
+									</Option>
+								);
+							})}
 						</Select>
 					</Flex>
 				</Container>
 			</Flex>
 
-			<Button variant="solid" onClick={() => setGameState(1)}>
+			<Button variant="solid" onClick={gameStart}>
 				game start
 			</Button>
 		</>
