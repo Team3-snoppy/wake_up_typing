1e9608c23176680025ca8470b8925620720a840b
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-02 17:05:31 +0900

    Fix score/sleep

--- server/routes/score.js
+++ server/routes/score.js
@@ -6,23 +6,19 @@ router.use(authCheck);
 
 router.post('/', async (req, res) => {
   const { gameScore, date } = req.body;
-  const { userId } = req.cookies;
-  console.log(gameScore, date, userId);
-  const resData = await db('score').insert(
+  const userId = req.user.id;
+  await db('score').insert(
     {
-      user_id: Number(userId),
+      user_id: userId,
       game_score: gameScore,
       create_at: date,
-    },
-    ['*'],
-    {
-      includeTriggerModifications: true,
     }
   );
   try {
-    res.status(201).json({ data: resData });
-  } catch {
-    res.status(400).json({ data: 'ボディが適切ではありません。' });
+    res.status(201).json({ data: "success score insert" });
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ data: 'Server Error' });
   }
 });
 
--- server/routes/sleep.js
+++ server/routes/sleep.js
@@ -2,22 +2,21 @@ const express = require('express');
 const router = express.Router();
 const authCheck = require('./../middleware/authCheck');
 const db = require('./../index');
-
 router.use(authCheck);
 
 router.post('/', async (req, res) => {
   const { sleepTime, date } = req.body;
-  const { userId } = req.cookies;
-  console.log(userId);
-  const resData = await db('sleeps').insert({
-    user_id: Number(userId),
+  const userId = req.user.id;
+  await db('sleeps').insert({
+    user_id: userId,
     sleep_time: sleepTime,
     create_at: date,
   });
   try {
-    res.status(201).json({ data: resData });
-  } catch {
-    res.status(400).json({ data: 'ボディが適切ではありません。' });
+    res.status(201).json({ data: 'success sleeptime insert' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ data: 'Server Error' });
   }
 });
 
