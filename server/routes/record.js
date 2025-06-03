cddd925ab99c91cb387861caf1e82d17975c2327
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-03 14:15:06 +0900

    output get records function

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,33 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authCheck = require('./../middleware/authCheck');
+const getMonthData = require('../utils/getMonthData')
 const db = require('./../index');
-const { startOfMonth, endOfMonth, format } = require('date-fns');
 router.use(authCheck);
 
 // こっちを先に書かないと”month”がパスパラメータ扱いになってエラーとなる
 router.get('/month', async (req, res) => {
-  const userId = req.user.id;
-  // const targetDate = new Date();
-  const targetDate = new Date('2025-05-01'); // debug用
-
-  const start = format(startOfMonth(targetDate), 'yyyy-MM-dd');
-  const end = format(endOfMonth(targetDate), 'yyyy-MM-dd');
-  
-  const resData = await db('score')
-    .innerJoin('sleeps', function () {
-      this.on('score.user_id', '=', 'sleeps.user_id')
-      .on('score.create_at','=','sleeps.create_at');
-    })
-    .where('score.user_id','=',userId)
-    .whereBetween('score.create_at', [start, end])
-    .select(
-      'score.id',
-      'score.user_id',
-      'score.game_score',
-      'sleeps.sleep_time',
-      'score.create_at'
-    );
+  const resData = await getMonthData(req);
   if (resData.length === 0) {
     return res.status(404).json({ data: '今月のレコードが見つかりません' });
   }
@@ -40,11 +20,14 @@ router.get('/:date', async (req, res) => {
   // create_atはDB保存時も検索時も日付のみでOKだが、取り出す時だけJSのDate型になるのでUTCで帰ってくる
   const resData = await db('score')
     .innerJoin('sleeps', function () {
-      this.on('score.user_id', '=', 'sleeps.user_id')
-      .on('score.create_at','=','sleeps.create_at');
+      this.on('score.user_id', '=', 'sleeps.user_id').on(
+        'score.create_at',
+        '=',
+        'sleeps.create_at'
+      );
     })
-    .where('score.user_id','=',userId)
-    .where('score.create_at','=', date)
+    .where('score.user_id', '=', userId)
+    .where('score.create_at', '=', date)
     .select(
       'score.id',
       'score.user_id',
@@ -59,7 +42,5 @@ router.get('/:date', async (req, res) => {
   }
   res.status(200).json({ data: resData });
 });
-
-
 
 module.exports = router;
---
+++ server/utils/getMonthData.js
@@ -0,0 +1,32 @@
+const { startOfMonth, endOfMonth, format } = require('date-fns');
+const db = require('./../index');
+
+const getMonthData = async (req) => {
+  const userId = req.user.id;
+  // const targetDate = new Date();
+  const targetDate = new Date('2025-05-01'); // debug用
+
+  const start = format(startOfMonth(targetDate), 'yyyy-MM-dd');
+  const end = format(endOfMonth(targetDate), 'yyyy-MM-dd');
+
+  const resData = await db('score')
+    .innerJoin('sleeps', function () {
+      this.on('score.user_id', '=', 'sleeps.user_id').on(
+        'score.create_at',
+        '=',
+        'sleeps.create_at'
+      );
+    })
+    .where('score.user_id', '=', userId)
+    .whereBetween('score.create_at', [start, end])
+    .select(
+      'score.id',
+      'score.user_id',
+      'score.game_score',
+      'sleeps.sleep_time',
+      'score.create_at'
+    );
+  return resData;
+};
+
+module.exports = getMonthData;
