1cac5a12cb40b035cececf9a909b730f3adc2aa0
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-05 10:27:33 +0900

    Fix 認証バグ

--- front/src/Pages/ScorePage.jsx
+++ front/src/Pages/ScorePage.jsx
@@ -1,8 +1,20 @@
 import GameEnd from '../component/GameEnd';
 import Appbar from '../component/Appbar';
 import { VStack } from '@yamada-ui/react';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
 
 const ScorePage = () => {
+	const navigate = useNavigate();
+  // 認証状態を取得し未認証ならtopへリダイレクトする
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('/api/auth/myInfo', { credentials: 'include' });
+      if (!res.ok) {
+        navigate('/');
+      }
+    })();
+  }, []);
   return (
 		<VStack h='100vh' overflowY='hidden'>
       <Appbar />
